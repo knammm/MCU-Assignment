6bb22eb040b7b0806f34a358b675fd28ea5c797d
Nam Nguyen <nam.nguyenolkmphy@hcmut.edu.vn>
2023-11-29 23:47:54 +0700

    Update global sources

--- MCU_Assignment/Core/Src/global.c
+++ MCU_Assignment/Core/Src/global.c
@@ -6,3 +6,19 @@
  */
 
 
+#include "global.h"
+
+int status = INIT;
+
+int timerTraffic1 = 0;
+int timerTraffic2 = 0;
+
+int durationRED = 5000;
+int durationYELLOW = 2000;
+int durationGREEN = 3000;
+
+int led_index = 0;
+int led_buffer[4] = {0};
+
+int buzzer_value = 0;
+int index_buzzer = 1;
