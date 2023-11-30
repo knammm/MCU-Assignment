66d393b80edf198c09744e157fda5f6f25590ecf
Nam Nguyen <nam.nguyenolkmphy@hcmut.edu.vn>
2023-11-30 15:47:12 +0700

    Update buzzer config in pedestrian

--- MCU_Assignment/Core/Src/fsm_pedestrian.c
+++ MCU_Assignment/Core/Src/fsm_pedestrian.c
@@ -9,6 +9,8 @@
 int pedestrianFlag = 0;
 int pedestrianStatus = INIT;
 int flagBuzzer = 0;
+int timerBuzzer = 500;
+int timerMinus = 50;
 
 // Function to handle the buzzer process
 void buzzerProcess(int value) {
@@ -22,6 +24,8 @@ void fsm_pedestrian_run() {
             // Initialization phase
             buzzerProcess(0);
             flagBuzzer = 0;
+            timerBuzzer = 500;
+            setTimer(3, 10);
             Pedestrian_setColor(OFF_LED);
 
             // Check for pedestrian signal
@@ -41,7 +45,7 @@ void fsm_pedestrian_run() {
             Pedestrian_setColor(AUTO_GREEN);
 
             // Check for conditions to transition back to INIT
-            if (timerFlag[2] == 1 ){
+            if (timerFlag[2] == 1){
             	pedestrianStatus = INIT;
             }
             else if(status == YELLOW_RED || status == GREEN_RED) {
@@ -51,12 +55,30 @@ void fsm_pedestrian_run() {
             // Buzzer process during green signal with specific conditions
             if (durationYELLOW < 5000) {
                 int needMore = 5 - (durationYELLOW / 1000);
-                if(timerTraffic2 <= needMore && status == RED_GREEN) flagBuzzer = 1;
-                if(flagBuzzer) buzzerProcess(512* (6 - (durationYELLOW / 1000) - timerTraffic2));
+                if((timerTraffic2 <= needMore && status == RED_GREEN) || status == RED_YELLOW) flagBuzzer = 1;
+                if(timerFlag[3] == 1){
+                	timerBuzzer = timerBuzzer - timerMinus;
+                	if(timerBuzzer <= 150)  timerBuzzer = 150;
+                	if(status == RED_YELLOW && timerTraffic2 <= 2) timerBuzzer = 100;
+                	if(flagBuzzer) buzzerProcess(32 * (6 - (durationYELLOW / 1000) - timerTraffic2));
+                	HAL_Delay(10);
+                	setTimer(3, timerBuzzer);
+
+                }
+                else buzzerProcess(0);
                 // Execute buzzer process if needed
             } else if (durationYELLOW >= 5000 && status == RED_YELLOW && timerTraffic2 <= 5) {
                 // Buzzer process during specific conditions in red-yellow transition
-                buzzerProcess(512 * (6 - timerTraffic2));
+                if(timerFlag[3] == 1){
+                	timerBuzzer = timerBuzzer - timerMinus;
+                	if(timerBuzzer <= 100) timerBuzzer = 100;
+                	buzzerProcess(32 * (6 - timerTraffic2));
+                	// Give a delay...
+                	HAL_Delay(10);
+                	setTimer(3, timerBuzzer);
+
+                }
+                else buzzerProcess(0);
             }
             // Buzzer for duration yellow < 5
             break;
@@ -65,6 +87,8 @@ void fsm_pedestrian_run() {
             // Automatic mode with red signal for pedestrians
             buzzerProcess(0);
             flagBuzzer = 0;
+            timerBuzzer = 500;
+            setTimer(3, 10);
             Pedestrian_setColor(AUTO_RED);
 
             // Check for conditions to transition back to INIT
