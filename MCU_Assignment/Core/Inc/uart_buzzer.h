a686813731527212fefd6ea8a8f41fbcd6f0f1bd
Nam Nguyen <nam.nguyenolkmphy@hcmut.edu.vn>
2023-11-30 15:46:58 +0700

    Update buzzer function by knammm

--- MCU_Assignment/Core/Inc/uart_buzzer.h
+++ MCU_Assignment/Core/Inc/uart_buzzer.h
@@ -8,6 +8,7 @@
 #ifndef INC_UART_BUZZER_H_
 #define INC_UART_BUZZER_H_
 
+#include "global.h"
 void buzzer();
 
 #endif /* INC_UART_BUZZER_H_ */
--- MCU_Assignment/Core/Src/uart_buzzer.c
+++ MCU_Assignment/Core/Src/uart_buzzer.c
@@ -4,7 +4,9 @@
  *  Created on: Nov 29, 2023
  *      Author: olkmphy
  */
+#include "uart_buzzer.h"
 
 void buzzer(){
 	// Implement buzzer here...
+	__HAL_TIM_SetCompare(&htim3, TIM_CHANNEL_1, buzzer_value);
 }
