79d5e83d7a52937ccb4e05e21a1d435c1b47f6c1
LAPTOP-88RA5TIU\ASUS <dat.nguyenitbk0506@hcmut.edu.vn>
2023-12-06 16:13:24 +0700

    new update

--- MCU_Assignment/Core/Src/fsm_manual.c
+++ MCU_Assignment/Core/Src/fsm_manual.c
@@ -51,8 +51,11 @@ void fsm_manual_run(){
 				timerTraffic1 = durationYELLOW / 1000;
 				timerTraffic2 = 3;
 			}
-//			HAL_UART_Transmit(&huart2, (void *)str, sprintf(str, "\r\n!7SEG1: %d#\r\n",timerTraffic1), 5);
-//			HAL_UART_Transmit(&huart2, (void *)str, sprintf(str, "\r\n!7SEG2: %d#\r\n\n\n",timerTraffic2), 5);
+			if(timerSecond == 1){
+				setTimerSecond(1000);
+				HAL_UART_Transmit(&huart2, (void *)str, sprintf(str, "\r\n!7SEG1: %d#\r\n",timerTraffic1), 500);
+				HAL_UART_Transmit(&huart2, (void *)str, sprintf(str, "\r\n!7SEG2: %d#\r\n\n\n",timerTraffic2), 500);
+			}
 			break;
 		case MOD_YELLOW:
 			if(timerFlag[1] == 1){
